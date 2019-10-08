e2075fc05f363f988e2a973ae8ca6de1cac7416d
Trey <tajohns22@gmail.com>
2019-10-08 15:35:29 -0500

    Updates

---
+++ precache-manifest.10a054d09306f69d5df6d1edba204ce6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8696061c8a53f7df5c11e6c9b2c860fb",
+    "url": "/monstersrolodex/index.html"
+  },
+  {
+    "revision": "9c956446e4e2fe74c6ea",
+    "url": "/monstersrolodex/static/css/main.994af998.chunk.css"
+  },
+  {
+    "revision": "b0703eff4582a78f493a",
+    "url": "/monstersrolodex/static/js/2.5075ebff.chunk.js"
+  },
+  {
+    "revision": "9c956446e4e2fe74c6ea",
+    "url": "/monstersrolodex/static/js/main.b4ccec18.chunk.js"
+  },
+  {
+    "revision": "041bbfae01e7605ae90a",
+    "url": "/monstersrolodex/static/js/runtime-main.372b9fdb.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/monstersrolodex/static/media/logo.25bf045c.svg"
+  }
+]);
